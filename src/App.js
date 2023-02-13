ee9c824a0de318e01b1f9b6d5475cba52750b60c
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-02-13 18:37:18 -0500

    creates the repo and does the exercise according to the instructions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article className='card__container'>
+        <img src='https://raw.githubusercontent.com/makeitrealcamp/top-v27/a731136682131a1657d794f22f37b56bdfbb3e63/onboarding/html-css/order-summary/images/illustration-hero.svg' alt='ilustration'/>
+        <section className='card__container-text'>
+          <h1>Order Summary</h1>
+          <p>You can now lister to millions of songs, audiobooks and podcasts on any device anywhere you like!</p>
+        </section>
+        <section className='charge__plan'>
+          <img src='https://raw.githubusercontent.com/makeitrealcamp/top-v27/a731136682131a1657d794f22f37b56bdfbb3e63/onboarding/html-css/order-summary/images/icon-music.svg' alt='music logo'/>
+          <section className='plan__time'>
+            <h4>Annual Plan</h4>
+            <p>$59.99/year</p>
+          </section>
+          <a className='change__link' href=''>Change</a>
+        </section>
+        <section className='footer'>
+          <button className='charge__button'> Proceed to Payment</button>
+          <a className='cancel__link' href=''>Cancel Order</a>
+        </section>
+      </article>
     </div>
   );
 }
